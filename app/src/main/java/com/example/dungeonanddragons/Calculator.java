6d78eda8838c570da6748ff07bd83e76519ca32d
GobDice <GobSot@yandex.ru>
2022-06-08 11:09:35 +0300

    GitInit 4

---
+++ app/src/main/java/com/example/dungeonanddragons/Calculator.java
@@ -0,0 +1,14 @@
+package com.example.dungeonanddragons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
